a1d4e6f2f0f7a0e6be24a7ca8519700388bd7765
Daniel Vo <daniel.vo@berkeley.edu>
2021-10-28 19:09:39 -0700

    get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{
+        model: Category,
+        attributes: ['category_name']
+      }, 
+      {
+        model: Tag, 
+        attributes: ['tag_name']
+      }
+      ]
+    });
+    console.log(productData);
+
+    const products = productData.map((product) => product.get({ plain: true}));
+    console.log("products", products);
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
